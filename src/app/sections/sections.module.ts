ea3a6d9f43cac2ba01fbc56059309f62f93dc139
progvar <prog.varadi@gmail.com>
2020-10-10 23:01:25 +0200

    feat: wrap the input inside a form element to support additions with pressing enter

--- src/app/sections/sections.module.ts
+++ src/app/sections/sections.module.ts
@@ -5,12 +5,21 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DetailsSectionComponent } from './details/details.component';
 import { NicknamesSectionComponent } from './nicknames/nicknames.component';
 
 @NgModule({
-    imports: [CommonModule, MatListModule, MatCardModule, MatButtonModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule],
+    imports: [
+        CommonModule,
+        MatListModule,
+        MatCardModule,
+        MatButtonModule,
+        MatFormFieldModule,
+        MatInputModule,
+        FormsModule,
+        ReactiveFormsModule,
+    ],
     declarations: [DetailsSectionComponent, NicknamesSectionComponent],
     exports: [DetailsSectionComponent, NicknamesSectionComponent],
 })
