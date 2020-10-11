bca63539ef65324f3a2adf587994b937c8d42d76
progvar <prog.varadi@gmail.com>
2020-10-11 02:35:15 +0200

    feat: remove the mat-divider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,12 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { VendorModule } from './vendor/vendor.module';
 import { SectionsModule } from './sections/sections.module';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [
-        BrowserModule,
-        BrowserAnimationsModule,
-        VendorModule,
-        SectionsModule,
-    ],
+    imports: [BrowserModule, BrowserAnimationsModule, SectionsModule],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
